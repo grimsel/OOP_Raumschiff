68d9a00185cc6a2fb2a84e67ee0d0cd4d78fa02a
Andrew <m8r-gvf11e@bobmail.info>
2016-03-06 00:34:31 +0100

    Raumschiff-Prototyp mit Firewall

--- Raumschiffe/RaumschiffVorlage.cs
+++ Raumschiffe/RaumschiffVorlage.cs
@@ -4,10 +4,13 @@
     {
         //Das Attribut 'geschwindigkeit' erhält zu Beginn den Wert 5.
         int geschwindigkeit = 5;
+        string schiffName;
 
-        //Das Attribut 'schiffName' ist zu Beginn leer. Es ist absichtich so definiert, dass es von überall 
-        //direkt angesprochen und verändert werden kann. Und zwar mit dem modifier 'public'
-        public string schiffName;
+        // Diese Methode gibt dem Raumschiff einen Namen.
+        public void setzeNamen(string neuerName)
+        {
+            this.schiffName = neuerName;
+        }
 
         // Diese Methode gibt auf der Konsole Informationen aus zum Raumschiff, z.B. Name und Geschwindigkeit.
         public void zeigeStatus()
