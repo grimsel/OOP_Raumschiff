4b66d6017c45687c23bdb9e3fefec7125b562947
Andrew <m8r-gvf11e@bobmail.info>
2016-03-06 23:25:16 +0100

    Raumschiff Echtzeit

--- Raumschiffe/Program.cs
+++ Raumschiffe/Program.cs
@@ -1,11 +1,61 @@
-﻿class Program
+﻿
+using System;
+
+class Program
 {
     static void Main(string[] args)
     {
         // Create an object of type RaumschiffVorlage.
         RaumschiffVorlage raumschiffEins = new RaumschiffVorlage();
+        RaumschiffVorlage raumschiffZwei = new RaumschiffVorlage();
 
         raumschiffEins.setzeNamen("alba");
         raumschiffEins.zeigeStatus();
+        raumschiffZwei.setzeNamen("duga");
+        raumschiffZwei.zeigeStatus();
+
+        int asteroidWucht;
+        int runde = 1;
+        Random Zufallszahlgenerator = new Random(); // initialisiert die Zufallsklasse
+
+        // 10 Runden durchspielen. Hier aufpassen, dass nicht eine Runde zuviel oder zu wenig gespielt wird.
+        while (runde <= 10)
+        {
+            Console.WriteLine("\n\r--- Runde {0} ---", runde);
+
+            //--- RaumschiffEins ---
+            if (raumschiffEins.exisitert())
+            {
+                // durchschnittlich jedes 3. Mal trifft ein Asteroid auf RaumschiffEins
+                if (Zufallszahlgenerator.Next(1, 3) == 1)
+                {
+                    // erzeuge eine zufällige Wucht (Masse) des Asteroiden 
+                    asteroidWucht = Zufallszahlgenerator.Next(1, 10);
+                    // ..und simuliere die Kollision von Asteroid und Raumschiff 
+                    Console.WriteLine("{0}: Kollision mit Wucht {1}", raumschiffEins.gibmirNamen(), asteroidWucht);
+                    raumschiffEins.kollision(asteroidWucht);
+                }
+            }
+
+            //--- RaumschiffZwei ---
+            if (raumschiffZwei.exisitert())
+            {
+                // durchschnittlich jedes 3. Mal trifft ein Asteroid auf RaumschiffEins
+                if (Zufallszahlgenerator.Next(1, 3) == 1)
+                {
+                    // erzeuge eine zufällige Wucht (Masse) des Asteroiden 
+                    asteroidWucht = Zufallszahlgenerator.Next(1, 10);
+                    // ..und simuliere die Kollision von Asteroid und Raumschiff 
+                    Console.WriteLine("{0}: Kollision mit Wucht {1}", raumschiffZwei.gibmirNamen(), asteroidWucht);
+                    raumschiffZwei.kollision(asteroidWucht);
+                }
+            }
+
+            //--- Anzeige der Raumschiffe un ihren Stati ---
+            raumschiffEins.zeigeStatus();
+            raumschiffZwei.zeigeStatus();
+
+            runde++;
+        }
     }
 }
--- Raumschiffe/RaumschiffVorlage.cs
+++ Raumschiffe/RaumschiffVorlage.cs
@@ -2,20 +2,57 @@
 
     class RaumschiffVorlage
     {
-        //Das Attribut 'geschwindigkeit' erhält zu Beginn den Wert 5.
+        //Das Attribut 'geschwindigkeit' erhält zu Beginn den Wert 5
         int geschwindigkeit = 5;
+        int schild = 2;
+        int leben = 10;
         string schiffName;
 
-        // Diese Methode gibt dem Raumschiff einen Namen.
+        // Diese Methode gibt dem Raumschiff einen Namen
         public void setzeNamen(string neuerName)
         {
             this.schiffName = neuerName;
         }
-
-        // Diese Methode gibt auf der Konsole Informationen aus zum Raumschiff, z.B. Name und Geschwindigkeit.
-        public void zeigeStatus()
+        // Diese Methode gibt den Namen des Raumschiffs zurück
+        public string gibmirNamen()
         {
-            Console.WriteLine("{0} hat speed {1}", this.schiffName, this.geschwindigkeit);
+            return this.schiffName;
+        }
+
+    // Diese Methode simuliert eine Kollision mit einem Asteoriden. Dabei der Wert 'Leben' neu berechnet.
+    public void kollision(int aufprall)
+        {
+            //prüfe ob der Asteroid unser Schutzschild durchschlagen hat
+            if (aufprall > this.schild )
+            {
+                //berechne den verbleibenden Aufprall nach der Dämpfung mit dem Schild
+                int rest_aufprall = aufprall - this.schild;
+                //reduziere die Lebenspunkte
+                this.leben -= rest_aufprall;
+                if (this.leben <= 0)
+                {
+                    Console.WriteLine("Raumschiff {0} wurde zerstört.", this.schiffName);
+                }
+            }
+        //Der Teil des Aufpralls, welcher nicht durch das Schild aufgefangen werden konnte.
+        }
+
+    // Diese Methode gibt auf der Konsole Informationen aus zum Raumschiff, z.B. Name und Geschwindigkeit.
+    public void zeigeStatus()
+    {
+        if (this.leben > 0)
+        {
+            Console.WriteLine("{0} hat speed {1} und noch {2} Leben", this.schiffName, this.geschwindigkeit, this.leben);
+        }
+        else
+        {
+            //Raumschiff gibts nicht mehr
         }
     }
+    public bool exisitert()
+    {
+        return (this.leben > 0);
+    }
+
+} // class
 
