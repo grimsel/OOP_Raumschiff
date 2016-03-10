6abd96db95f36bc41671216f739e7bd91a4a71a7
Andrew <m8r-gvf11e@bobmail.info>
2016-03-11 00:23:38 +0100

    Project'Klavier' added

---
+++ Klavier/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        KlavierVorlage mySteinway = new KlavierVorlage();
+        mySteinway.musiziere();
+        mySteinway.deckelOeffnen();
+        mySteinway.musiziere();
+    }
+    }
+
+
+class KlavierVorlage
+{
+    string[] tasten = { "c", "d" };
+    string lied1 = "Oh when the saint's...";
+    bool deckelOffen = false;
+
+
+    public void deckelOeffnen()
+    {
+        Console.WriteLine("öffne Deckel...");
+        deckelOffen = true;
+    }
+
+    public void deckelSchliessen()
+    {
+        deckelOffen = false;
+    }
+
+    public void musiziere() 
+    {
+        Console.WriteLine("Ich erhalte die Anweisung zu musizieren"); 
+        if (deckelOffen) { Console.WriteLine(lied1); } else { Console.WriteLine("Kann nicht musizieren, da Deckel zu"); }
+    }
+    
+}
