9693bea26b3604a5d5da0b2d8ceb7b4192b63c8f
Andrew <m8r-gvf11e@bobmail.info>
2016-03-15 07:55:25 +0100

    Klavier optimiert nach erster FW

--- Klavier/Program.cs
+++ Klavier/Program.cs
@@ -1,43 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class Program
 {
     static void Main(string[] args)
     {
         KlavierVorlage mySteinway = new KlavierVorlage();
+        KlavierVorlage mySabel =    new KlavierVorlage();
         mySteinway.musiziere();
+        mySteinway.nameGeben("Steinway");
         mySteinway.deckelOeffnen();
         mySteinway.musiziere();
+        mySabel.musiziere();
+        Console.ReadKey(); //Tastendruck abwarten
     }
 }
 
 
 class KlavierVorlage
 {
-    string[] tasten = { "c", "d" };
-    string lied1 = "Oh when the saint's...";
-    bool deckelOffen = false;
-
-
+    //Attribute setzen
+    string  AttrKlavierName = "Unbekannt"; //Die Vorlage kennt den Namen noch nicht
+    string  AttrLied1 = "Oh when the saint's..."; //Dieses Lied soll musiziert werden
+    bool    AttrDeckelOffen = false; //Zustand des Klavierdeckels:
+                                     //true = offen; false=zu                 
     public void deckelOeffnen()
     {
-        Console.WriteLine("öffne Deckel...");
-        deckelOffen = true;
+        Console.WriteLine("Klavier " + AttrKlavierName + " öffnet den Deckel");
+        AttrDeckelOffen = true;
     }
 
     public void deckelSchliessen()
     {
-        deckelOffen = false;
+        AttrDeckelOffen = false;
     }
 
     public void musiziere() 
     {
-        Console.WriteLine("Ich erhalte die Anweisung zu musizieren"); 
-        if (deckelOffen) { Console.WriteLine(lied1); } else { Console.WriteLine("Kann nicht musizieren, da Deckel zu"); }
+        Console.WriteLine("Klavier "+AttrKlavierName+ " erhält die Anweisung zu musizieren");
+        //Wenn der Klavierdeckel offen ist, soll das Lied ausgegeben werden, sonst eine Fehlermeldung.
+        if (AttrDeckelOffen) { Console.WriteLine(AttrLied1); } else { Console.WriteLine("Kann nicht musizieren, da Deckel zu\n\r"); }
     }
-    
-}
+
+    //Methode mit Eingabe-Parameter 'tempName'. Im Unterschied zu einem Attribut existiert 'tempName' nur kurze Zeit, 
+    //naemlich waehrend dem Durchlauf dieser Methode.
+    public void nameGeben(string tempName)
+    {
+        AttrKlavierName = tempName;
+        Console.WriteLine("Klavier erhält den Namen "+ AttrKlavierName);
+    }
+} // Ende von class KlavierVorlage
